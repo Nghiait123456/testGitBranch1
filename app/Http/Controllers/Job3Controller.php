b402ae73f5b1dc73171a1621307ed8f87d49d25f
nghiaPm <minhnghia.pham.it@gmail.com>
2021-03-14 12:26:06 +0700

    add controller

---
+++ app/Http/Controllers/Job3Controller.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Jobs\Log1;
+use App\Jobs\Log2;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class Job3Controller extends Controller
+{
+    //
+
+    public function job1(){
+        for( $i = 0; $i <10; $i ++){
+//            Log::info('testest');
+            Log1::dispatch()->onQueue('fast-1');
+
+
+
+
+            echo('test horizon');
+        }
+//        for( $i = 0; $i <30; $i ++) {
+//            Log2::dispatch()->onQueue('fast-1');
+//        }
+    }
+
+    public function job2() {
+//        for( $i = 0; $i <3; $i ++){
+//            Log1::dispatch()->onQueue('default');
+//            Log1::dispatch()->onqueue('hight');
+//            Log1::dispatch()->onqueue('low');
+//        }
+
+        for( $i = 0; $i <3; $i ++){
+            Log1::dispatch()->onQueue('default');
+            Log1::dispatch()->onqueue('hight');
+            Log1::dispatch()->onqueue('low');
+        }
+        echo('tets test test');
+        echo('done');
+        echo('done');
+
+    }
+}
