45f05d37d599db7fdf44f063732c250d2c322e1d
nghiaPm <minhnghia.pham.it@gmail.com>
2021-03-14 09:44:06 +0700

    edit job2

--- app/Http/Controllers/TestHorizonController.php
+++ app/Http/Controllers/TestHorizonController.php
@@ -27,10 +27,17 @@ class TestHorizonController extends Controller
     }
 
     public function job2() {
+//        for( $i = 0; $i <3; $i ++){
+//            Log1::dispatch()->onQueue('default');
+//            Log1::dispatch()->onqueue('hight');
+//            Log1::dispatch()->onqueue('low');
+//        }
+
         for( $i = 0; $i <3; $i ++){
             Log1::dispatch()->onQueue('default');
             Log1::dispatch()->onqueue('hight');
             Log1::dispatch()->onqueue('low');
         }
+        echo('tets test test');
     }
 }
